e6b4882ea1b7bd59f66cc2423a5d30ecda94c8af
rafael-rodrigues95 <fael_rs@live.com>
2023-05-15 11:40:29 -0300

    Commit inicial do projeto.

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+export default function App1(){
+  return(
+    <View>
+      <Text>Novo App</Text>
+    </View>
+  );
+};
