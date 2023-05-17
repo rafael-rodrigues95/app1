fcb91f698fa23235df6ca0d38d1c64dc6592fd2e
rafael-rodrigues95 <fael_rs@live.com>
2023-05-17 11:27:58 -0300

    Adicionando imagem e botão comum.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,54 @@
 import React from 'react';
 
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, Button, Alert} from 'react-native';
 import CompUm from './components/comp1';
 import Caixas from './components/Caixas';
 import Estilos from './styles/style.js';
+
+/* Exemplos de functions
+
+--------------------------------
+function dobro(n){
+  return n*2
+}
+
+(n)=>{return n*2}
+
+n=>return n*2 
+
+const dobro=n=>n*2
+--------------------------------
+
+--------------------------------
+function soma(n1,n2){
+  return n1+n2
+}
+
+(n1,n2)=>{return n1+n2}
+--------------------------------
+
+--------------------------------
+const metade=(n)=>{
+  return n/2
+}
+
+<Text>{metade(4)}</Text>
+--------------------------------
+
+*/
 
 export default function App1() {
   let maybeExibir = true;
   return (
     
       <View style={Estilos.conteiner}>
-        <Image source={require('./assets/logo-cam.jpg')} style={estilos.logo} />
-      
+        {/* <Image source={require('./assets/logo-cam.jpg')} style={estilos.logo} /> */}
+        
+        <Button
+          title="Mostrar mensagem"
+          onPress={()=>Alert.alert('Objetivo','Curso de React Native.')}
+        />
+
         <Text style={{color: '#f00'}}>Cursos</Text>
         <CompUm curso="React Native" nota="9.0" />
         <CompUm curso="Java" nota="8.5" />
@@ -45,6 +82,4 @@ const estilos = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   }
-});
-
-*/
+});*/
