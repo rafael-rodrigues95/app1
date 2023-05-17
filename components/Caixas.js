5842878f999cd58121c00d51adb9354080e0b9e6
rafael-rodrigues95 <fael_rs@live.com>
2023-05-17 08:53:29 -0300

    Adicionando flexWrap e flexGros aos estilos.

--- components/Caixas.js
+++ components/Caixas.js
@@ -8,10 +8,26 @@ import {
 
 export default function(props){
     return(
-        <View style={{width:'100%', flexDirection: 'row'}}>
+        /*<View style={{width:'100%', flexDirection: 'row'}}>
             <View style={{width:'33.33%',height:50,backgroundColor:'#00f'}}></View>
             <View style={{width:'33.33%',height:50,backgroundColor:'#00a'}}></View>
             <View style={{width:'33.33%',height:50,backgroundColor:'#005'}}></View>
+        </View>*/
+        /*<View style={{width:'100%', height: 300, flexDirection: 'row', flexWrap: 'wrap'}}>
+            <View style={{width: 85, height: 50, backgroundColor:'#00f'}}></View>
+            <View style={{width: 85, height: 50, backgroundColor:'#00a'}}></View>
+            <View style={{width: 85, height: 50, backgroundColor:'#005'}}></View>
+            <View style={{width: 85, height: 50, backgroundColor:'#f00'}}></View>
+            <View style={{width: 85, height: 50, backgroundColor:'#a00'}}></View>
+            <View style={{width: 85, height: 50, backgroundColor:'#500'}}></View> 
+        </View>*/
+        <View style={{width:'100%', height: 300, flexDirection: 'column', justifyContent: 'center', alignItems: 'stretch'}}>
+            <View style={{flexGrow: 1, backgroundColor:'#00f'}}></View>
+            <View style={{flexGrow: 2, backgroundColor:'#00a'}}></View>
+            <View style={{flexGrow: 3, backgroundColor:'#005'}}></View>
+            <View style={{flexGrow: 1, backgroundColor:'#f00'}}></View>
+            <View style={{flexGrow: 2, backgroundColor:'#a00'}}></View>
+            <View style={{flexGrow: 3, backgroundColor:'#500'}}></View>            
         </View>
     )
 }
